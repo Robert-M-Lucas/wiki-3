cfd71a2dff97ada8d2e824c04fd310978418f990
Robert-App-Dev <robert01.appdev@gmail.com>
2023-08-11 17:35:44 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::time::Instant;
 use num_format::{Locale, ToFormattedString};
 use rusqlite::Connection;
@@ -17,94 +17,93 @@ fn main() {
     visited.insert(starting_at.to_string());
 
     //? Consider linked list
-    let mut next_layer = Vec::new();
-    let mut current_layer = vec![Page {
+    let mut open_set = VecDeque::with_capacity(1_000_000);
+    open_set.push_back(Page {
         page: String::from(starting_at),
         from: Vec::new(),
-    }];
+    });
 
-    let mut count = 0;
+    let mut count: u32 = 0;
 
     'main_loop: loop {
-        'page_loop: for mut page in current_layer.into_iter() {
-            count += 1;
-            if count % 1000 == 0 {
-                println!(
-                    "Pages searched: {} | Cache size: {} | Next layer size: {}",
-                    count.to_formatted_string(&Locale::en),
-                    visited.len().to_formatted_string(&Locale::en),
-                    next_layer.len().to_formatted_string(&Locale::en)
-                );
+        let mut page = open_set.pop_front().unwrap();
+
+        count += 1;
+        if count % 1000 == 0 {
+            println!(
+                "Pages searched: {} [{:?}/page] | Cache size: {} | Open set size: {}",
+                count.to_formatted_string(&Locale::en),
+                start_time.elapsed() / count,
+                visited.len().to_formatted_string(&Locale::en),
+                open_set.len().to_formatted_string(&Locale::en)
+            );
+        }
+
+        let links: String;
+        loop {
+            if page.page.as_str() == searching_for {
+                println!("{}", page.to_str());
+                break 'main_loop;
             }
 
-            let links: String;
-            loop {
-                if page.page.as_str() == searching_for {
-                    println!("{}", page.to_str());
-                    break 'main_loop;
-                }
+            let mut res = cached_query.query_row(
+                (&page.page,),
+                |row| Ok((row.get(1).unwrap(), row.get(2).unwrap()))
+            );
 
-                let mut res = cached_query.query_row(
+            if res.is_err() {
+                let mut v: Vec<char> = page.page.chars().collect();
+                v[0] = v[0].to_uppercase().nth(0).unwrap();
+                page.page = v.into_iter().collect();
+
+                res = db.query_row(
+                    "SELECT * FROM page_references WHERE title = ?",
                     (&page.page,),
                     |row| Ok((row.get(1).unwrap(), row.get(2).unwrap()))
                 );
-
-                if res.is_err() {
-                    let mut v: Vec<char> = page.page.chars().collect();
-                    v[0] = v[0].to_uppercase().nth(0).unwrap();
-                    page.page = v.into_iter().collect();
-
-                    res = db.query_row(
-                        "SELECT * FROM page_references WHERE title = ?",
-                        (&page.page,),
-                        |row| Ok((row.get(1).unwrap(), row.get(2).unwrap()))
-                    );
-                }
-
-                if let Err(e) = res {
-                    // println!("Fetching {:?}->{} failed with: {:?}", page.from.last(), page.page, e);
-                    continue 'page_loop;
-                }
-
-                let (links_, redirect): (String, bool) = res.unwrap();
-
-                if redirect {
-                    if links_.is_empty() {
-                        continue 'page_loop;
-                    }
-                    page.add_to_path(links_);
-                    continue;
-                }
-
-                links = links_;
-                break;
             }
 
-            for link in links.split("<|>") {
-                if link.is_empty() {
-                    continue;
-                }
-
-                if !visited.insert(link.to_string()) {
-                    continue;
-                }
-
-                if link == searching_for {
-                    page.add_to_path(link.to_string());
-                    println!("{}", page.to_str());
-                    break 'main_loop;
-                }
-
-                next_layer.push(page.add_to_path_clone(link.to_string()));
+            if let Err(_e) = res {
+                // println!("Fetching {:?}->{} failed with: {:?}", page.from.last(), page.page, e);
+                continue 'main_loop;
             }
+
+            let (links_, redirect): (String, bool) = res.unwrap();
+
+            if redirect {
+                if links_.is_empty() {
+                    continue 'main_loop;
+                }
+                page.add_to_path(links_);
+                continue;
+            }
+
+            links = links_;
+            break;
         }
 
-        if next_layer.is_empty() {
+        for link in links.split("<|>") {
+            if link.is_empty() {
+                continue;
+            }
+
+            if !visited.insert(link.to_string()) {
+                continue;
+            }
+
+            if link == searching_for {
+                page.add_to_path(link.to_string());
+                println!("{}", page.to_str());
+                break 'main_loop;
+            }
+
+            open_set.push_back(page.add_to_path_clone(link.to_string()));
+        }
+
+        if open_set.is_empty() {
             println!("No more pages!");
             break;
         }
-        current_layer = next_layer;
-        next_layer = Vec::new();
     }
 
     println!("Completed in {:?}", start_time.elapsed());
